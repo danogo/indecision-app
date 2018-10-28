2a048fd76439b502c61ff66881b1b3ad4bf9b8c1
Daniel Ogonowski <daniel.ogonowski@gmail.com>
2018-10-28 19:48:49 +0100

    configured babel with webpack

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,6 @@
-import { isAdult, canDrink } from './person';
+import isSenior, { isAdult, canDrink } from './person';
 
 console.log('entry.js is running, hooray! homies');
 console.log('isAdult(18)', isAdult(18));
 console.log('canDrink(20)', canDrink(20));
-
+console.log('isSenior(68)', isSenior(68));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/entry.js',
+  // entry point for webpack
+  entry: './src/app.js',
+  // plac where it give us bundled file
   output: {
+    // it has to be absolute path, we use __dirname - built-in node current path and path.join package(without installing, already available with node)
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
   devServer: {
+    // place from what to serve via webpack-dev-server
     contentBase: './public'
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
 }
